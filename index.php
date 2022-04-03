76aaf6ebf72f71732d004086e69527b5c429d594
Gabriel Nalini <gabrielnalini03@gmail.com>
2022-04-03 16:07:45 -0300

    Testando Arrays no PHP :)

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $numeroDaMegaSena = [24, 23, 22, 21, 20];
+        echo "Número 1: ".$numeroDaMegaSena[0]."</br>";
+        echo "Número 2: ".$numeroDaMegaSena[1]."</br>";
+        echo "Número 3: ".$numeroDaMegaSena[2]."</br>";
+        echo "Número 4: ".$numeroDaMegaSena[3]."</br>";
+        echo "Número 5: ".$numeroDaMegaSena[4]."</br>";
+    ?>
+</body>
+</html>
